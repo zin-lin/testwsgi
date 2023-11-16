fff97f11725cf2d8988645426c047ba1928f9d7d
zin-lin <zinlinhtun34@gmail.com>
2023-11-16 19:23:04 +0000

    sec commit

--- generate-waitress-service.sh
+++ generate-waitress-service.sh
@@ -11,7 +11,7 @@ Type=simple
 User=root
 Group=root
 WorkingDirectory=/home/$USER/xray
-ExecStart=/home/$USER/xray/bin/waitress-serve --listen=127.0.0.1:5000 app:app
+ExecStart=/home/$USER/xray/bin/waitress-serve --listen=127.0.0.1:8080 app:app
 Restart=always
 
 [Install]
